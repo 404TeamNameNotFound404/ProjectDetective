86cf0af0bd5c743372e522280ce079e58105f68c
BPA98 <bpaone.dev@gmail.com>
2024-11-08 20:44:33 +0100

    Publish

--- Source/ProjectDetective/Detective/DetectivePhotocamera/DetectivePhotocamera.cpp
+++ Source/ProjectDetective/Detective/DetectivePhotocamera/DetectivePhotocamera.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "../PlayerController/Detective.h"
 #include "DetectivePhotocamera.h"
+#include "../PlayerController/Detective.h"
 
 
 // Sets default values
